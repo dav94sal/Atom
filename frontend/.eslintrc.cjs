c8b90c8a773db21bd6eec20acbd9813b5347a07b
david <dav94sal@gmail.com>
2024-10-18 19:58:53 -0500

    Initialize Express and Sequelize with CSRF protection

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/prop-types': 'off' // turn off rule that checks if prop types are always validated (not needed for AA projects)
+  },
+  ignorePatterns: ['dist', 'node_modules'] // will ignore files in dist and node_modules
+}
---
+++ hidden/backend-setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+# npm install --save "cookie-parser" "cors" "csurf" "dotenv" \
+#                    "express" "express-async-errors" "helmet" \
+#                    "jsonwebtoken" "morgan" "per-env" "sequelize@6" \
+#                    "sequelize-cli@6" "pg"
+
+# npm install --save-d 'sqlite3' 'dotenv-cli' 'nodemon'
+
+# echo "All required packages and dependencies installed successfully!"
+mkdir config
+
+cd config
+
+touch index.js
+
+
