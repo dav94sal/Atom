0ab746859484cf1a2a78b60c6f449b275ce0c86c
david <dav94sal@gmail.com>
2024-10-24 16:37:53 -0500

    Add user log in backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
-
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { restoreUser } = require('../../utils/auth.js');
 
 router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
 
 // GET /api/set-token-cookie
 const { setTokenCookie } = require('../../utils/auth.js');
