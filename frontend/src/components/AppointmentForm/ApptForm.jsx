c96dbd70a64c5b7473dd789ac26e991b8517fcd0
david <dav94sal@gmail.com>
2024-11-06 16:45:01 -0600

    Add appointment form

--- frontend/src/components/AppointmentForm/ApptForm.jsx
+++ frontend/src/components/AppointmentForm/ApptForm.jsx
@@ -1,15 +1,43 @@
+import { useDate } from '../../context/DateContext';
 import './appt-form.css'
 
 function ApptForm({ day, hour }) {
+    const { currentDate } = useDate()
 
+    console.log(`${currentDate.year}-${day}-${currentDate.month}`)
     // Need to convert hours!
 
+    const calculateHour = () => {
+        const timeArr = hour.split(' ')
+
+        if (timeArr[1] === 'PM') {
+            timeArr[0] = parseInt(timeArr[0]) + 12
+        }
+        return `${timeArr[0]}:00`
+    }
+
     return (
-        <>
-            <h2>Appointment Form!</h2>
-            <p>{ day }</p>
-            <p>{ hour }</p>
-        </>
+        <div className='form-container'>
+            <form>
+                <input
+                    type='text'
+                    placeholder='Add title'
+                />
+                <input
+                    type='date'
+                    defaultValue={`${currentDate.year}-${day}-${currentDate.month}`}
+                />
+                <input
+                    type='time'
+                    defaultValue={() => calculateHour() - 1}
+                />
+                <input
+                    type='time'
+                    defaultValue={calculateHour()}
+                />
+                <p>{ hour }</p>
+            </form>
+        </div>
     )
 }
 
